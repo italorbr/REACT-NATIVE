8012742a74985ebcda20668713610d45889a9330
italorbr <italorosa.profissional@gmail.com>
2023-08-14 01:39:39 -0300

    v1.01

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 import { TextInput, View, Text, Button } from "react-native";
+import ResultImc from "./ResultImc/"
 
 export default function Form(){
+
+const [height, setHeight]=useState(null)
+const [weight, setWeight]=useState(null)
+const [messageImc, setMessageImc]=useState("Preencha o peso e a altura")
+const [imc, setImc]=useState(null)
+const [textButton, setTextButton]=useState("Calcular")
+
+function imcCalculator(){
+    return setImc((weight/(height*height)).toFixed(2))
+}
+
+function validationImc(){
+    if(weight != null && height != null){
+        imcCalculator()
+        setHeight(null)
+        setWeight(null)
+        setMessageImc("Seu imc eh igual: ")
+        setTextButton("Calcular novamente")
+        return
+    }
+    setImc(null)
+    setTextButton("Calcular")
+    setMessageImc("Preencha o peso e a altura")
+}
+
     return(
         <View>
             <View>
                 <Text>Altura</Text>
-                <TextInput placeholder="Ex. 1.65" keyboardType="numeric"/>
+                <TextInput 
+                onChangeText={setHeight}
+                value={height}
+                placeholder="Ex. 1.65" 
+                keyboardType="numeric"
+                />
+
                 <Text>Peso</Text>
-                <TextInput placeholder="EX. 60" keyboardType="numeric"/>
-                <Button title="Calcular IMC"/>
+                <TextInput 
+                onChangeText={setWeight}
+                value={weight}
+                placeholder="EX. 60" 
+                keyboardType="numeric"
+                />
+                <Button
+                onPress={() => validationImc()} 
+                title={textButton}
+                />
             </View>
             <ResultImc messageResultImc={messageImc} resultImc={imc}/>
         </View>
